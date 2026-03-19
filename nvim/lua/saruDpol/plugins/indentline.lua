4b3eeb3a629c62f62df35558e1765270a2721b84
saruDpol <ptorne3@gmail.com>
2026-03-19 20:10:33 +0100

    python indenting recognizes for, if, while, ...

--- nvim/lua/saruDpol/plugins/indentline.lua
+++ nvim/lua/saruDpol/plugins/indentline.lua
@@ -3,17 +3,19 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	main = "ibl",
 	config = function()
-		-- Define custom scope highlight groups
 		local hooks = require("ibl.hooks")
+
+		-- highlight groups
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 			vim.api.nvim_set_hl(0, "ScopeRed", { fg = "#cb4a15" })
 			vim.api.nvim_set_hl(0, "ScopePurple", { fg = "#6c71c4" })
 			vim.api.nvim_set_hl(0, "ScopeGreen", { fg = "#859901" })
 			vim.api.nvim_set_hl(0, "ScopeGold", { fg = "#ffc102" })
 		end)
+
 		require("ibl").setup({
 			indent = {
-				char = "▏",
+				char = vim.fn.nr2char(0x258F),
 			},
 			scope = {
 				enabled = true,
@@ -27,9 +29,22 @@ return {
 				show_end = false,
 				show_exact_scope = true,
 				injected_languages = true,
+
+				-- ? FIX FOR PYTHON
+				include = {
+					node_type = {
+						python = {
+							"if_statement",
+							"for_statement",
+							"while_statement",
+							"function_definition",
+							"class_definition",
+							"with_statement",
+							"try_statement",
+						},
+					},
+				},
 			},
 		})
-
-		-- Set indent color to gold
 	end,
 }
